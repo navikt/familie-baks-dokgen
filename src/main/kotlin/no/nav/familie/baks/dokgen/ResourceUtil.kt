5b02d3789fa4f6925a8571726fe13c30dd63fc29
Stig Strøm <stig.strom@gmail.com>
2026-01-20 15:13:14 +0100

    attach jsonschem draft07 (#258)
    * Preregistrerer json-schema draft 07 slikt at den ikke lastes ned ved validering
    
    * Sletter filer som ikke lenger er i bruk

--- src/main/kotlin/no/nav/familie/baks/dokgen/ResourceUtil.kt
+++ src/main/kotlin/no/nav/familie/baks/dokgen/ResourceUtil.kt
@@ -6,8 +6,6 @@ import java.net.URL
 internal object ResourceUtil {
     fun getTemplateContent(templateName: String): String = getResource("/templates/$templateName/template.hbs").readText()
 
-    fun getSchemaJsonAsString(templateName: String): String = getResource("/templates/$templateName/schema.json").readText()
-
     fun getSchemaJsonAsString(format: DocFormat): String = getResource("/formats/$format/schema.json").readText()
 
     fun getHeader(format: DocFormat): String = getResource("/formats/$format/header.html").readText()
@@ -17,4 +15,6 @@ internal object ResourceUtil {
     fun getStylesheet(format: DocFormat): String = getResource("/formats/$format/style.css").readText()
 
     private fun getResource(location: String): URL = this::class.java.getResource(location) ?: throw FileNotFoundException(location)
+
+    fun getDraft07SchemaAsString(): String = getResource("/formats/draft-schema7.json").readText()
 }
